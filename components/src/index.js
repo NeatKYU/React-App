55887eb0d71b9d4a7301d43f2e97f8d00fd2aa15
ninjasKYU <2066apple@naver.com>
2020-12-18 13:47:23 +0900

    second project done

--- components/src/index.js
+++ components/src/index.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail 
-                name="Sam" 
-                date="2019-2-12"
-                text="fuck you!"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail 
-                name="Lala" 
-                date="2020-1-30"
-                text="tell me fuck"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail 
-                name="Jane" 
-                date="2020-10-13"
-                text="ok let's move out"
-                avatar={faker.image.image()}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          name="Sam"
+          date="2019-2-12"
+          text="fuck you!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name="Lala"
+          date="2020-1-30"
+          text="tell me fuck"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name="Jane"
+          date="2020-10-13"
+          text="ok let's move out"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
