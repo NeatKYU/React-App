fe53df9f1da6aca2907ce97c4da3de60dbb17c0a
ninjasKYU <2066apple@naver.com>
2020-12-21 11:15:38 +0900

    create pics project and components

---
+++ pics/src/components/App.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+	return <div><SearchBar/></div>
+}
+
+export default App;
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+class SearchBar extends react.Component {
+	render() {
+		return (
+			<div>
+				<form>
+					<input type="text"/>
+				</form>
+			</div>
+		)
+	}
+};
+
+export default SearchBar;
