6258727571d78cfc0fc719a82769c3701a2b936a
ninjasKYU <2066apple@naver.com>
2020-12-21 15:28:23 +0900

    create submit fucntion and understand about this

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,34 @@
 import react from 'react';
 
 class SearchBar extends react.Component {
+
+	state = { term: '' };
+
+/*
+	onInputChange(e){
+		console.log(e.target.value);
+	}
+ */
+	onFormSubmit = e => {
+		e.preventDefault();
+		
+		console.log(this.state.term)
+	}
+
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form 
+					className="ui form" 
+					onSubmit={this.onFormSubmit}
+				>
 					<div className="field">
 						<label>Image Search</label>
-						<input type="text"/>
+						<input 
+							type="text" 
+							value={this.state.term}
+							onChange={(e) => this.setState({ term: e.target.value })}
+						/>
 					</div>
 				</form>
 			</div>
