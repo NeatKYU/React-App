cd365ac82d635cab3b0c0084532d00ffa71d8f09
kyu <you@example.com>
2020-12-22 14:00:37 +0900

    get image file and refactoring axios API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,25 +1,33 @@
 import react from 'react';
 import axios from 'axios';
+import unsplash from '../API/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends react.Component{
-	
-	onSearchSubmit(term){
-		console.log(term)
-		axios.get('https://api.unsplash.com/search/photos', {
+
+	state = { images: [] };
+
+	/* 화살표 함수 인수에 http요청일 때 async붙이기  */
+	onSearchSubmit = async (term) => {
+		
+		const response = await unsplash.get('/search/photos', {
 			params: {
 				query: term
 			},
-			headers: {
-				Authorization: 'Client-ID lJNRounV4ABqUJ_BmIs-QmkfXXbOvzPxPcBFhdcofec'
-			}
 		})
+		// 비동기 async를 사용하기전 이렇게 콜백함수로 받아와서 순서를 보장받아야함
+		// async await를 사용하면 이런거 할 필요 없음
+		//.then((response) => console.log(response.data.results))
+
+		this.setState({ images: response.data.results })
+		//console.log(response.data.results);
 	}
 
 	render(){
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit}/>
+				Found: {this.state.images.length} images
 			</div>
 		)
 	}
