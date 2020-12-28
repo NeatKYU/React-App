f27a5f54c2363f87b6ffe2d639747a0d2cefcf75
kyu <you@example.com>
2020-12-28 13:16:38 +0900

    widgets init

---
+++ widgets/src/App.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import Accordion from './components/Accordion'
+
+export default () => {
+    return <h1><Accordion></Accordion></h1>
+};
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const Accordion = () => {
+    return <h1>Accordion</h1>
+}
+
+export default Accordion;
