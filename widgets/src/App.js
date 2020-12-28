2365334100100cd8b57e53f8197a18eab1f9ac67
kyu <you@example.com>
2020-12-28 14:30:10 +0900

    use Hooks(useState)

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,21 @@
 import react from 'react';
 import Accordion from './components/Accordion'
 
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: "How do you use React?",
+        content: 'You use React by creating components'
+    }
+]
+
 export default () => {
-    return <h1><Accordion></Accordion></h1>
+    return <h1><Accordion items={items}></Accordion></h1>
 };
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,42 @@
-import react from 'react';
+import react, { useState } from 'react';
 
-const Accordion = () => {
-    return <h1>Accordion</h1>
+const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    }
+
+    //react.Freagment는 div같은 태그 없이 값을 넘겨주고 싶을 때 사용하는듯
+    const renderedItem = items.map((item, index) => {
+        
+        const active = index === activeIndex ? 'active' : '';
+
+        return (
+            <react.Fragment key={item.title}>
+                <div 
+                //왜 {onTitleClick(index)}라고하면 안될까?
+                //왜 에로우 함수를 써야하는가???
+                // 그냥함수 쓰면 무한루프에 빠진다 강의에선 클릭하기전에 발동해서 안쓴다는데....
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(index)} 
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`content ${active}`}>
+                    <p>{item.content}</p>
+                </div>
+            </react.Fragment>
+        )
+    })
+
+    return (
+        <div className="ui styled accordion">
+            {renderedItem}
+            <h1>{activeIndex}</h1>
+        </div>
+    )
 }
 
 export default Accordion;
