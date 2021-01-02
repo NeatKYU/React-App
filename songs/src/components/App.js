21f76613042ba67d116259ad6e2207b78812f0f8
ninjasKYU <2066apple@naver.com>
2021-01-02 15:33:14 +0900

    create song project use redux

---
+++ songs/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { selectsong } from '../actions';
+
+const App = () => {
+	return <div>Hello React World</div>
+}
+
+export default App;
---
+++ songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "부기맨", duration: "4:05" },
+    { title: "Very Good", duration: "2:30" },
+    { title: "okey dokey", duration: "3:15" },
+    { title: "Shall We Dance", duration: "1:45" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
