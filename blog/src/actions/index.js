29795bb4b918d47a36e9a7ec4ac3cf14b48c8ee6
kyu <you@example.com>
2021-01-04 13:43:27 +0900

    understand redux and apply redux-thunk on action creater

---
+++ blog/src/actions/index.js
@@ -0,0 +1,23 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+/* 
+export const fetchPosts = () => {
+    //그냥 하면 action return값은 js형태여야함
+    //redux-thunk를 사용하면 return값으로 함수도 가능
+    return function(dispatch, getState){
+        const promise = jsonPlaceholder.get('/posts')
+
+        return {
+            type: 'FETCH_POSTS',
+            payload: promise
+        };
+    }
+    
+}; */
+// -------------->> 간소화
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts')
+
+    dispatch({ type: 'FETCH_POSTS', payload: response })
+}
