7bc85e0f503053bd935138e8edb3a0a2388f4708
ninjasKYU <2066apple@naver.com>
2021-01-11 21:31:05 +0900

    update fetchPostsAndUsers action createor

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -17,19 +17,29 @@ export const fetchPosts = () => {
 }; */
 // -------------->> 간소화
 
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  await dispatch(fetchPosts());
+}
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
-/* 
+
 export const fetchUsers = (id) => async (dispatch) => {
   // user를 개별적으로 가져오기위해 id를 넣어줌
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USERS", payload: response.data });
 };
- */
+
+
+ //memoize를 사용해서 같은 userid를 가지면
+ // 딱 한번만 요청되도록 할 수 있다.
+ // 하지만 나중에 userid정보가 바뀌어서 다시 불러와야 할 때
+ // 그러지 못한다는 단점이 있다. 그래서 완벽한 해결책은 아니다.
+/* 
 export const fetchUsers = (id) => (dispatch) => {
   _fetchUser(id, dispatch);
 };
@@ -39,3 +49,4 @@ const _fetchUser = _.memoize( async (id, dispatch) => {
 
   dispatch({ type: "FETCH_USERS", payload: response.data });
 });
+ */
